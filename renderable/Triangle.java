3a83aab3e74b7a7bf03350d577cecab6dfc4856c
Eduard Sauter <eduard.sauter@gmail.com>
2017-11-03 14:59:14 +0100

    general renderable objects and squares and triangles

---
+++ renderable/Triangle.java
@@ -0,0 +1,27 @@
+package renderable;
+import shader.ShaderPipeline;
+import java.nio.*;
+import renderable.*;
+import static org.lwjgl.system.MemoryUtil.*;
+import org.lwjgl.BufferUtils;
+
+public class Triangle extends Renderable {
+    public Triangle (ShaderPipeline pipeline) {
+        FloatBuffer vb = BufferUtils.createFloatBuffer(2 * 3);
+        float h = (float)Math.sqrt(3.0);
+        vb.put(-1.0f).put(-h/3);
+        vb.put(+1.0f).put(-h/3);
+        vb.put(0.0f).put(2.0f*h/3.0f);
+        vb.flip();
+
+        FloatBuffer uvb = BufferUtils.createFloatBuffer(2 * 3);
+        uvb.put(0.0f).put(1.0f);
+        uvb.put(1.0f).put(1.0f);
+        uvb.flip();
+
+        IntBuffer ib = BufferUtils.createIntBuffer(6);
+        ib.put(0).put(1).put(2);
+        ib.flip();
+        super.setupVertexArray(pipeline, vb, uvb, ib);
+    }
+}
